2d33b9e6ee19dfdc2515ac60d9a42cf46fcf739a
Sergio Dacal Rodriguez <sdacalrodriguez@gmail.com>
2024-09-17 17:41:21 +0200

    feat: include tailwindCSS into project

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import './App.css'
 
 function App() {
-  return <h1>Panel de control de Sensores - Stark Industries</h1>
+  return (
+    <h1 className='text-3xl font-bold underline'>
+      Panel de control de Sensores - Stark Industries
+    </h1>
+  )
 }
 
 export default App
